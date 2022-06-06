ac8e9a9736b83cda4f5b0bbdbb3665332615eb47
Barış Günenç <power.of.baris@gmail.com>
2022-06-06 12:33:58 +0300

    jquery autocomplete

---
+++ SanTsgHotelBooking.Web/Controllers/SearchController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SanTsgHotelBooking.Data.Repository.IRepository;
+
+namespace SanTsgHotelBooking.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ILogger<SearchController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SearchController(ILogger<SearchController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- SanTsgHotelBooking.Web/wwwroot/js/site.js
+++ SanTsgHotelBooking.Web/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    $("#searchBox").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: '/api/search',
+                headers: {
+                    "RequestVerificationToken":
+                        $('input[name="__RequestVerificationToken"]').val()
+                },
+                data: { "term": request.term },
+                dataType: "json",
+                success: function (data) {
+                    response($.map(data, function (item) {
+                        return item;
+                    }))
+                },
+                error: function (xhr, textStatus, error) {
+                    alert(xhr.statusText);
+                },
+                failure: function (response) {
+                    alert("failure " + response.responseText);
+                }
+            });
+
+        },
+        select: function (e, i) {
+            //func
+        },
+        minLength: 3
+    });
+});
